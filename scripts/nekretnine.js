cf181c457309c37a8153c2eae3b0f77ed6945612
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-11-27 00:01:45 +0100

    Spriala 2 Zadatak 2 + izbrisan primjer za SpisakNekretnina

---
+++ scripts/nekretnine.js
@@ -0,0 +1,64 @@
+function spojiNekretnine(divReferenca, instancaModula, tip_nekretnine) { 
+
+    let spisakNekretnina = instancaModula.filtrirajNekretnine({ tip_nekretnine: tip_nekretnine });
+    let tipNekretnine = tip_nekretnine;
+    if(tipNekretnine === "Stan"){
+        tipNekretnine = "stan";
+    } else if(tipNekretnine === "Kuća"){
+        tipNekretnine = "kuca";
+    }else if(tipNekretnine === "Poslovni prostor"){
+        tipNekretnine = "pp";
+    }
+    for(let key of spisakNekretnina){
+        divReferenca.innerHTML += `
+        <div id="${tipNekretnine}">
+            <p>${tip_nekretnine}</p><br>
+            <img src="" alt="Slika" /><br>
+            <p class="nazivNekretnine">Naziv nekretine: ${key["naziv"]}</p>
+            <p class="kvadratura">Kvadratura: ${key["kvadratura"]}</p>
+            <p class="cijena">Cijena: ${key["cijena"]}</p>
+            <div class="detaljiDugme">
+                <input type="button" value="Detalji">
+            </div>
+        </div>`;
+    }
+}
+
+const div = document.getElementById("wrapper");
+
+const listaNekretnina = [{ 
+    id: 1, 
+    tip_nekretnine: "Kuća", 
+    naziv: "Useljiv stan Sarajevo", 
+    kvadratura: 18, cijena: 100000, 
+    tip_grijanja: "plin", 
+    lokacija: "Novo Sarajevo", 
+    godina_izgradnje: 2019, 
+    datum_objave: "01.10.2023.", 
+    opis: "Sociis natoque penatibus.", 
+    upiti: [{ korisnik_id: 1, tekst_upita: "Nullam eu pede mollis."}
+    ,{
+    korisnik_id: 2, tekst_upita: "Phasellus viverra nulla."
+    }]}, {
+    id: 2,
+     tip_nekretnine: "Kuća",
+      naziv: "Mali poslovni prostor",
+       kvadratura: 30,
+        cijena: 150000,
+         tip_grijanja: "struja",
+          lokacija: "Centar",
+           godina_izgradnje: 2005,
+            datum_objave: "20.08.2023.",
+             opis: "Magnis dis parturient montes.",
+              upiti: [{ korisnik_id: 2, tekst_upita: "Integer tincidunt."
+} ]
+}]
+
+const listaKorisnika = [{ id: 1, ime: "Neko", prezime: "Nekic", username: "username1",
+}, {
+id: 2, ime: "Neko2", prezime: "Nekic2", username: "username2",
+}]
+
+let nekretnine = SpisakNekretnina();
+nekretnine.init(listaNekretnina, listaKorisnika);
+spojiNekretnine(div, nekretnine, "Kuća");
