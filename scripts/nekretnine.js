28854872722b327f301fd258736f32cbb90681cb
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2023-11-28 10:05:45 +0100

    Formatiranje koda i izmjena za nekretnine

--- scripts/nekretnine.js
+++ scripts/nekretnine.js
@@ -3,16 +3,15 @@ function spojiNekretnine(divReferenca, instancaModula, tip_nekretnine) {
     let spisakNekretnina = instancaModula.filtrirajNekretnine({ tip_nekretnine: tip_nekretnine });
     let tipNekretnine = tip_nekretnine;
     if(tipNekretnine === "Stan"){
-        tipNekretnine = "stan";
+        tipNekretnine = "stan-bc";
     } else if(tipNekretnine === "Kuća"){
-        tipNekretnine = "kuca";
+        tipNekretnine = "kuca-bc";
     }else if(tipNekretnine === "Poslovni prostor"){
-        tipNekretnine = "pp";
+        tipNekretnine = "pp-bc";
     }
     for(let key of spisakNekretnina){
         divReferenca.innerHTML += `
         <div id="${tipNekretnine}">
-            <p>${tip_nekretnine}</p><br>
             <img src="" alt="Slika" /><br>
             <p class="nazivNekretnine">Naziv nekretine: ${key["naziv"]}</p>
             <p class="kvadratura">Kvadratura: ${key["kvadratura"]}</p>
@@ -24,12 +23,14 @@ function spojiNekretnine(divReferenca, instancaModula, tip_nekretnine) {
     }
 }
 
-const div = document.getElementById("wrapper");
+const stanDiv = document.getElementById("stan");
+const kucaDiv = document.getElementById("kuca");
+const ppDiv = document.getElementById("pp");
 
 const listaNekretnina = [{ 
     id: 1, 
     tip_nekretnine: "Kuća", 
-    naziv: "Useljiv stan Sarajevo", 
+    naziv: "Kuca u Sarajevu", 
     kvadratura: 18, cijena: 100000, 
     tip_grijanja: "plin", 
     lokacija: "Novo Sarajevo", 
@@ -41,18 +42,45 @@ const listaNekretnina = [{
     korisnik_id: 2, tekst_upita: "Phasellus viverra nulla."
     }]}, {
     id: 2,
-     tip_nekretnine: "Kuća",
-      naziv: "Mali poslovni prostor",
-       kvadratura: 30,
-        cijena: 150000,
-         tip_grijanja: "struja",
-          lokacija: "Centar",
-           godina_izgradnje: 2005,
-            datum_objave: "20.08.2023.",
-             opis: "Magnis dis parturient montes.",
-              upiti: [{ korisnik_id: 2, tekst_upita: "Integer tincidunt."
-} ]
-}]
+    tip_nekretnine: "Kuća",
+    naziv: "Kuca u Sarajevu",
+    kvadratura: 30,
+    cijena: 150000,
+    tip_grijanja: "struja",
+    lokacija: "Centar",
+    godina_izgradnje: 2005,
+    datum_objave: "20.08.2023.",
+    opis: "Magnis dis parturient montes.",
+    upiti: [{ korisnik_id: 2, tekst_upita: "Integer tincidunt."
+    } ]
+    },{ 
+    id: 3, 
+    tip_nekretnine: "Stan", 
+    naziv: "Useljiv stan Sarajevo", 
+    kvadratura: 18, cijena: 100000, 
+    tip_grijanja: "plin", 
+    lokacija: "Novo Sarajevo", 
+    godina_izgradnje: 2019, 
+    datum_objave: "01.10.2023.", 
+    opis: "Sociis natoque penatibus.", 
+    upiti: [{ korisnik_id: 1, tekst_upita: "Nullam eu pede mollis."}
+    ,{
+    korisnik_id: 2, tekst_upita: "Phasellus viverra nulla."
+    }]},
+{ 
+    id: 4, 
+    tip_nekretnine: "Poslovni prostor", 
+    naziv: "Mali poslovni prostor", 
+    kvadratura: 18, cijena: 100000, 
+    tip_grijanja: "plin", 
+    lokacija: "Novo Sarajevo", 
+    godina_izgradnje: 2019, 
+    datum_objave: "01.10.2023.", 
+    opis: "Sociis natoque penatibus.", 
+    upiti: [{ korisnik_id: 1, tekst_upita: "Nullam eu pede mollis."}
+    ,{
+    korisnik_id: 2, tekst_upita: "Phasellus viverra nulla."
+    }]}]
 
 const listaKorisnika = [{ id: 1, ime: "Neko", prezime: "Nekic", username: "username1",
 }, {
@@ -61,4 +89,6 @@ id: 2, ime: "Neko2", prezime: "Nekic2", username: "username2",
 
 let nekretnine = SpisakNekretnina();
 nekretnine.init(listaNekretnina, listaKorisnika);
-spojiNekretnine(div, nekretnine, "Kuća");
+spojiNekretnine(kucaDiv, nekretnine, "Kuća");
+spojiNekretnine(stanDiv, nekretnine, "Stan");
+spojiNekretnine(ppDiv, nekretnine, "Poslovni prostor");
