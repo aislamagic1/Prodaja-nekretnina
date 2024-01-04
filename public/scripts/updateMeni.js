3096f3dd78f4eb8a37de00d42ce748068f05c777
aislamagic1 <115921908+aislamagic1@users.noreply.github.com>
2024-01-04 22:55:06 +0100

    Update meni

--- public/scripts/updateMeni.js
+++ public/scripts/updateMeni.js
@@ -1,7 +1,12 @@
 function updatePrijavaLink() {
     PoziviAjax.getKorisnik((err, data) =>{
         if (!err && data) {
-            document.getElementById('prijava').innerText = 'Odjava';
+            document.getElementById('meniID').innerHTML = `
+            <li> <a class="link" href="http://localhost:3000/profil.html" target="_top">Profil</a> </li>
+            <li> <a class="link" href="http://localhost:3000/nekretnine.html" target="_top">Nekretnine</a> </li>
+            <li> <a class="link" href="http://localhost:3000/detalji.html" target="_top">Detalji</a> </li>
+            <li> <a class="link" href="http://localhost:3000/prijava.html" target="_top" id="prijava" onclick="odjavaLink()">Odjava</a> </li>
+            `;
         }else{
             console.log(err);
         }
